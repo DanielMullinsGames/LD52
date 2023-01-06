41ffe18f414528c1c76fe13ae404f30ae814cb70
Daniel Mullins <daniel.mullins.games@gmail.com>
2023-01-06 11:43:27 -0800

    Added "DMG Uber Shader" + basic graphics for noise, gradients, etc.

--- Assets/Addons/DanielMullinsGames/TimedBehaviour.cs
+++ Assets/Addons/DanielMullinsGames/TimedBehaviour.cs
@@ -3,22 +3,35 @@ using System.Collections;
 
 public class TimedBehaviour : ManagedBehaviour
 {
+    public float FrequencyMultiplier { get; set; } = 1f;
     public override bool UpdateWhenPaused => unscaledTime;
 
     [Header("Timer Parameters")]
-	public float originalFrequency;
-	public float volatility;
     public bool unscaledTime;
+
+    protected float OriginalFrequency => originalFrequency;
+    [SerializeField]
+	private float originalFrequency;
+
+    [SerializeField]
+	private float volatility;
 
 	protected float timer;
 	private float frequency;
 
     protected virtual bool Paused { get { return false; } }
+    private float TriggerTime => frequency * FrequencyMultiplier;
 
-	public void AddDelay(float delay)
+
+    public void AddDelay(float delay)
 	{
 		timer -= delay;
 	}
+
+    public void SetFrequency(float newFrequency)
+    {
+        frequency = newFrequency;
+    }
 
     public override void ManagedLateUpdate()
     {
@@ -30,7 +43,7 @@ public class TimedBehaviour : ManagedBehaviour
             }
 
             timer += unscaledTime ? Time.unscaledDeltaTime : Time.deltaTime;
-            if (timer > frequency)
+            if (timer > TriggerTime)
             {
                 float frequencyAdjust = (-0.5f + Random.value) * volatility;
                 frequency = originalFrequency + frequencyAdjust;
@@ -39,6 +52,11 @@ public class TimedBehaviour : ManagedBehaviour
             }
         }
 	}
+
+    protected void SkipToEndOfTimer()
+    {
+        timer = TriggerTime;
+    }
 
     protected void Reset()
     {
